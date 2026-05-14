016756c4463c68bd93e664d1941384454f7a50fc
Sherill Cornel <sherneldon@gmail.com>
2026-05-14 16:05:48 +0300

    fix frontend deployment

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST"]
+}));
 app.use(express.json());
 app.use(express.static(__dirname + "/public"));
 
