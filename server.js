2be606fd99d4cd1fd845664b006ba15903714a86
Sherill Cornel <sherneldon@gmail.com>
2026-05-15 08:33:24 +0300

    fix timezone to Kenya

--- server.js
+++ server.js
@@ -21,8 +21,8 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306
-  
+  port: process.env.DB_PORT || 3306,
+  timezone: '+03:00'
 });
 
 db.connect((err) => {
